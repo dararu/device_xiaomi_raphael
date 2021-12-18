ac70dbb87704b84214de5143eb783767b1122a2f
TH779 <i@779.moe>
2021-12-18 22:27:58 +0800

    raphael: Switch to prebuilt IFAAService
     * Starting from Android 12, non-root namespaces will
       no longer be allowed to use the PRODUCT_BOOT_JARS flag,
       which will cause org.ifaa.android.manager to not work.
       Without creating a new repository separately, we can
       also import the prebuilt IFAAService from stock ROM
       to make Alipay fingerprint payment work properly, which
       also reduces the difficulty of our subsequent maintenance.
    
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Ib1640e5cae6dcbb09e53af6037ddf966ed988d23

--- device.mk
+++ device.mk
@@ -247,10 +247,6 @@ PRODUCT_PACKAGES += \
     init.xiaomi.rc \
     ueventd.qcom.rc
 
-# IFAA manager
-PRODUCT_PACKAGES += \
-    IFAAService
-
 # IPACM
 $(call inherit-product, vendor/qcom/opensource/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
 
