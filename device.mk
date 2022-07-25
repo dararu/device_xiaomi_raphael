0d2f5a3216cd8354ca0a51d04a646047e1b9e70e
Joey Huab <joey@evolution-x.org>
2022-07-25 10:30:15 +0000

    Revert "raphael: Grant android.permission.WRITE_DEVICE_CONFIG to gms"
    This reverts commit d1ddcb3d7e00158a17c2720a6e9f3654758c7d63.

--- device.mk
+++ device.mk
@@ -205,10 +205,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
-# GMS Permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-gms.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp.permissions-gms.xml
-
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
